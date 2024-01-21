6b32d1f6ac3b707a675eea6cb365c222019a891b
Dmitry Permyakov <taren4ik@mail.ru>
2024-01-21 16:50:30 +1000

    db add .gitignore

--- app.py
+++ app.py
@@ -315,5 +315,5 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=os.environ.get('FLASK_SERVER_PORT', 5050),
-        static_folder='static'
+ #       static_folder='static'
     )
